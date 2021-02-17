d5f2f96bf582481d0d261f9c7d13042311ef7d65
yashbodhe <71684249+yashbodhe@users.noreply.github.com>
2021-02-17 21:42:17 +0530

    first commit

--- first/settings.py
+++ first/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'kcru_+_*+xp*eh9gcis8t9c_^-18%2xo&l2fh6y0mun!znga1d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['anuradhabank.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
